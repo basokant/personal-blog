bba8de76b60d7cde3930f7685131ebcfb8773b29
basokant <basokanthan@gmail.com>
2022-07-16 23:57:07 -0400

    migrate back to next-mdx-remote

--- components/youtube.tsx
+++ components/youtube.tsx
@@ -1,17 +1,24 @@
 import Video from 'react-youtube';
 
-const YouTube = ({ videoId }: {videoId: string}) => {
+const YouTube = ({ url }: {url: string}) => {
+  const videoID = youtube_parser(url);
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 0,
-        },
-    };
+  const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+      },
+  };
 
-    return <Video videoId={videoId} opts={opts}/>
+  return <Video videoId={videoID} opts={opts}/>
+}
+
+function youtube_parser(url: string): string{
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return (match&&match[7].length==11)? match[7] : "";
 }
 
 export default YouTube;
