a6be5d0640352c8d2f2d215e8a1820acfd47930d
basokant <basokanthan@gmail.com>
2022-07-29 21:50:01 -0400

    add simple page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { motion } from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+      exit: {
+        opacity: 0
+      }
+    }}>
+      <Component {...pageProps} />
+    </motion.div>
+  )
 }
 
 export default MyApp
