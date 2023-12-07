cd97693fae578c471b488c6b1aeec36ac1173ccb
basokant <basokanthan@gmail.com>
2023-12-07 15:50:42 -0500

    update resume

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,42 +3,42 @@ import Image from 'next/image';
 import { Mail } from 'react-feather';
 
 type HeroProps = {
-    isDesktop: boolean
+  isDesktop: boolean
 }
 
-const Hero = ({isDesktop}: HeroProps) => {
+const Hero = ({ isDesktop }: HeroProps) => {
 
-    return (
-        <>
-        <div className={styles.container}>
-            <div className={styles.col1}>
-                <h1 className={styles.welcome}><span className={styles.blue}>Hey!</span> I'm Ben.</h1>
-                <p className={styles.desc}>I'm a computer science student and aspiring software developer who loves designing, learning, and creating.</p>
-                <a className={styles.btn} href="mailto:basokanthan@gmail.com">
-                    <Mail className={styles.mailIcon} strokeWidth="3px"/>{ isDesktop && "Let's Chat" }
-                </a>
-                <a className={styles.link} href="/basokant_resume_august2022.pdf" target="_blank" rel="noreferrer">PDF Resumé</a>
-                <br />
-                <a href="https://github.com/basokant" target="_blank" rel="noreferrer">
-                    <img className={styles.socialBtn} src="/github.svg" alt="My GitHub" />
-                </a>
-                <a href="https://www.linkedin.com/in/benjamin-asokanthan/" target="_blank" rel="noreferrer">
-                    <img className={styles.socialBtn} src="/linkedin.svg" alt="My LinkedIn" />
-                </a>
-            </div>
-            { isDesktop &&
-                <div className={styles.col2}>
-                    <Image className={styles.image} src="/self_portrait.svg" width={396*1.1} height={592*1.1} ></Image>
-                </div>
-            }
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <h1 className={styles.welcome}><span className={styles.blue}>Hey!</span> I'm Ben.</h1>
+          <p className={styles.desc}>I'm a computer science student and aspiring software developer who loves designing, learning, and creating.</p>
+          <a className={styles.btn} href="mailto:basokanthan@gmail.com">
+            <Mail className={styles.mailIcon} strokeWidth="3px" />{isDesktop && "Let's Chat"}
+          </a>
+          <a className={styles.link} href="/basokant_resume.pdf" target="_blank" rel="noreferrer">PDF Resumé</a>
+          <br />
+          <a href="https://github.com/basokant" target="_blank" rel="noreferrer">
+            <img className={styles.socialBtn} src="/github.svg" alt="My GitHub" />
+          </a>
+          <a href="https://www.linkedin.com/in/benjamin-asokanthan/" target="_blank" rel="noreferrer">
+            <img className={styles.socialBtn} src="/linkedin.svg" alt="My LinkedIn" />
+          </a>
         </div>
-        { !isDesktop &&
-            <div className={styles.col2}>
-                <Image className={styles.image} src="/self_portrait.svg" width={360*1.1} height={592*1.1} ></Image>
-            </div>
+        {isDesktop &&
+          <div className={styles.col2}>
+            <Image className={styles.image} src="/self_portrait.svg" width={396 * 1.1} height={592 * 1.1} ></Image>
+          </div>
         }
-        </>
-    )
+      </div>
+      {!isDesktop &&
+        <div className={styles.col2}>
+          <Image className={styles.image} src="/self_portrait.svg" width={360 * 1.1} height={592 * 1.1} ></Image>
+        </div>
+      }
+    </>
+  )
 
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,38 +5,39 @@ import Link from 'next/link';
 import useViewport from '../hooks/useViewport';
 
 type NavbarProps = {
-    isDesktop: boolean
-    background?: "bg1" | "bg2"
-    sticky?: boolean
+  isDesktop: boolean
+  background?: "bg1" | "bg2"
+  sticky?: boolean
 }
 
-const Navbar = ( {isDesktop, background, sticky}: NavbarProps ) => {
+const Navbar = ({ isDesktop, background, sticky }: NavbarProps) => {
 
-    return (
-        <nav id={background} className={styles.container} 
-            style={
-                sticky && isDesktop? {
-                position: "sticky",
-                top: 0}: {}
-            }
-        >
-            <Logo marginRight={isDesktop ? "20px" : "0px"}/>
+  return (
+    <nav id={background} className={styles.container}
+      style={
+        sticky && isDesktop ? {
+          position: "sticky",
+          top: 0
+        } : {}
+      }
+    >
+      <Logo marginRight={isDesktop ? "20px" : "0px"} />
 
-            { isDesktop && 
-                <div className={styles.links}>
-                    <Link className={styles.link} href="/blog">Blog</Link>
-                    <a className={styles.link} href="/basokant_resume_august2022.pdf" target="_blank" rel="noreferrer">Resumé</a>
-                </div>
-            }
+      {isDesktop &&
+        <div className={styles.links}>
+          <Link className={styles.link} href="/blog">Blog</Link>
+          <a className={styles.link} href="/basokant_resume.pdf" target="_blank" rel="noreferrer">Resumé</a>
+        </div>
+      }
 
-            { !isDesktop && 
-                <div className={styles.links}>
-                    <Link className={styles.link} href="/blog">Blog</Link>
-                    <a className={styles.link} href="/basokant_resume_august2022.pdf" target="_blank" rel="noreferrer">Resumé</a>
-                </div>
-            }
-        </nav>
-    );
+      {!isDesktop &&
+        <div className={styles.links}>
+          <Link className={styles.link} href="/blog">Blog</Link>
+          <a className={styles.link} href="/basokant_resume.pdf" target="_blank" rel="noreferrer">Resumé</a>
+        </div>
+      }
+    </nav>
+  );
 }
 
 export default Navbar;
